433599a06b3972983801b9c7b85be01eeb79276f
Oliwia <czarownicanamiotle@gmail.com>
2020-11-04 21:58:23 +0100

    Controller, view, service created.

--- src/ExBook/Controllers/WishListController.cs
+++ src/ExBook/Controllers/WishListController.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ExBook.Extensions;
+using ExBook.Models.Authentication;
+using ExBook.Services;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
 
 namespace ExBook.Controllers
 {
     public class WishListController : Controller
     {
+        private readonly WishListService wishListService;
+
+        public WishListController(WishListService wishListService)
+        {
+            this.wishListService = wishListService;
+        }
+
+        [HttpGet]
+        [Route("/wishlist")]
+        [AllowAnonymous]
         public IActionResult Index()
         {
-            return View();
+            return this.HttpContext.User.Identity.IsAuthenticated
+                ? this.View()
+                : this.RedirectToSearch() as IActionResult;
         }
+
     }
 }
