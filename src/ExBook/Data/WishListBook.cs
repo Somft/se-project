d8db4c8a3c6c0fa18db5fd2ab0bb3b28b202a9f7
Bartek Piura <Bartek9805@hotmail.com>
2020-11-05 21:33:17 +0100

    feat: updated model

---
+++ src/ExBook/Data/WishListBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExBook.Data
+{
+    [Table("wish_list_book")]
+    public partial class WishListBook
+    {
+        [Key]
+        [Column("id")]
+        public Guid Id { get; set; }
+        [Column("wish_list_id")]
+        public Guid? WishListId { get; set; }
+        [Column("book_id")]
+        public Guid? BookId { get; set; }
+
+        [ForeignKey(nameof(BookId))]
+        [InverseProperty("WishListBook")]
+        public virtual Book Book { get; set; }
+        [ForeignKey(nameof(WishListId))]
+        [InverseProperty("WishListBook")]
+        public virtual WishList WishList { get; set; }
+    }
+}
