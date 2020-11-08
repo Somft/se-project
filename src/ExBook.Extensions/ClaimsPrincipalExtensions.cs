094609e70672206bb5054bd655f67fd84d48e061
Bartek Piura <Bartek9805@hotmail.com>
2020-11-08 14:48:45 +0100

    feat: updated model, adding user id to JWT

---
+++ src/ExBook.Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace ExBook.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetClaim(this ClaimsPrincipal principal, string claim)
+        {
+            return principal.Claims.FirstOrDefault(c => c.Type == claim)?.Value;
+        }
+
+        public static Guid? GetId(this ClaimsPrincipal principal)
+        {
+            string? idClaim = principal.GetClaim(ClaimTypes.Sid);
+
+            if (idClaim != null && Guid.TryParse(idClaim, out Guid result))
+            {
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- src/ExBook/Data/BookShelf.cs
+++ src/ExBook/Data/BookShelf.cs
@@ -1,28 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
 
 namespace ExBook.Data
 {
-    [Table("book_shelf")]
     public partial class BookShelf
     {
         public BookShelf()
         {
-            BookShelfBook = new HashSet<BookShelfBook>();
+            BookShelfBooks = new HashSet<BookShelfBook>();
         }
 
-        [Key]
-        [Column("id")]
         public Guid Id { get; set; }
-        [Column("user_id")]
         public Guid UserId { get; set; }
 
-        [ForeignKey(nameof(UserId))]
-        [InverseProperty("BookShelf")]
         public virtual User User { get; set; }
-        [InverseProperty("BookShelf")]
-        public virtual ICollection<BookShelfBook> BookShelfBook { get; set; }
+        public virtual ICollection<BookShelfBook> BookShelfBooks { get; set; }
     }
 }
