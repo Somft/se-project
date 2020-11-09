bb1463699cb7eff25ee301639b1a647888b2ce67
Bartek Piura <Bartek9805@hotmail.com>
2020-11-09 16:33:45 +0100

    feat: added missing relations

--- src/ExBook/Data/BookShelfBook.cs
+++ src/ExBook/Data/BookShelfBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 #nullable disable
 
@@ -6,6 +7,12 @@ namespace ExBook.Data
 {
     public partial class BookShelfBook
     {
+        public BookShelfBook()
+        {
+            this.InitiatorTransactions = new HashSet<Transaction>();
+            this.RecipientTransactions = new HashSet<Transaction>();
+        }
+
         public Guid Id { get; set; }
         public Guid? Photo { get; set; }
         public Guid BookId { get; set; }
@@ -13,5 +20,7 @@ namespace ExBook.Data
 
         public virtual Book Book { get; set; }
         public virtual BookShelf BookShelf { get; set; }
+        public virtual ICollection<Transaction> InitiatorTransactions { get; set; }
+        public virtual ICollection<Transaction> RecipientTransactions { get; set; }
     }
 }
