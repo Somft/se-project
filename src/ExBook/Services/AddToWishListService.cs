9ede688f14b366683e6d64a6e5e21c1b84664111
Oliwia <czarownicanamiotle@gmail.com>
2021-01-06 14:41:49 +0100

    Added API modifications to wishlist

--- src/ExBook/Services/AddToWishListService.cs
+++ src/ExBook/Services/AddToWishListService.cs
@@ -1,5 +1,7 @@
 ﻿using ExBook.Data;
 using ExBook.Models.AddToWhishList;
+using ExBook.OpenLibrary;
+using ExBook.OpenLibrary.Dto;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -13,10 +15,12 @@ namespace ExBook.Services
     public class AddToWishListService
     {
         private readonly ApplicationDbContext applicationDbContext;
+        private readonly OpenLibraryClient openLibraryClient;
 
-        public AddToWishListService(ApplicationDbContext applicationDbContext)
+        public AddToWishListService(ApplicationDbContext applicationDbContext, OpenLibraryClient openLibraryClient)
         {
             this.applicationDbContext = applicationDbContext;
+            this.openLibraryClient = openLibraryClient;
         }
         
         public async Task<bool> AddBook(AddToWishListViewModel book, Guid? user)
@@ -35,8 +39,8 @@ namespace ExBook.Services
 
             if (bok != null) //book already exists
             {
-               
-                if( await this.applicationDbContext.WishListBooks.AnyAsync(b => b.WishListId ==  userWishList.Id && b.BookId == bok.Id)) // exists in wishlist? throw error
+
+                if (await this.applicationDbContext.WishListBooks.AnyAsync(b => b.WishListId == userWishList.Id && b.BookId == bok.Id)) // exists in wishlist? throw error
                 {
                     return false;
                 }
@@ -52,23 +56,48 @@ namespace ExBook.Services
             }
             else // book doesnt exists, add to Book and wishlist
             {
-                bok = new Book()
+                var bookAPI = await SearchBook(book.Name, book.Author);
+                Book bok2 = this.applicationDbContext.Books.FirstOrDefault(b => b.Name == bookAPI.Title);
+                if (bok2 == null) //book doesnt exists in database
                 {
-                    Id = Guid.NewGuid(),
-                    Name = book.Name,
-                    Author = book.Author,
-                    Created = DateTime.Parse(book.Created)
+                    bok = new Book()
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = bookAPI.Title ?? book.Name,
+                        Author = bookAPI.Authors.FirstOrDefault().Name ?? book.Author,
+                        Created = bookAPI.FirstPublishDate ?? DateTime.Parse(book.Created),
+                        CoverUrl = bookAPI.Covers.FirstOrDefault().ToString() ?? null,
+                        Isbn = bookAPI.Key ?? null
+                    };
+                    this.applicationDbContext.Books.Add(bok);
 
-                };
-                this.applicationDbContext.Books.Add(bok);
-
-                userWishList.WishListBooks.Add(new WishListBook()
+                    userWishList.WishListBooks.Add(new WishListBook()
+                    {
+                        Id = Guid.NewGuid(),
+                        BookId = bok.Id
+                    });
+                }
+                else //book exists in database
                 {
-                    Id = Guid.NewGuid(),
-                    BookId = bok.Id
-                }) ;
+                    if (await this.applicationDbContext.WishListBooks.AnyAsync(b => b.WishListId == userWishList.Id && b.BookId == bok2.Id)) // exists in wishlist? throw error
+                    {
+                        return false;
+                    }
+                    else // if no, add to wishlist
+                    {
+                        userWishList.WishListBooks.Add(new WishListBook()
+                        {
+                            Id = Guid.NewGuid(),
+                            BookId = bok2.Id
+                        });
+
+                    }
+
+
+                }
+
+                
             }
-
             await this.applicationDbContext.SaveChangesAsync();
 
             return true;
@@ -86,6 +115,11 @@ namespace ExBook.Services
             await this.applicationDbContext.SaveChangesAsync();
 
             return true;
+        }
+
+        public async Task<OpenLibraryBook?> SearchBook(string? title, string? author)
+        {
+            return await openLibraryClient.SearchBook(title, author);
         }
     }
 }
