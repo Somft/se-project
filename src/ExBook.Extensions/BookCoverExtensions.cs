bdf47eba029c81fa2071d0fbe8f701b98983770a
Łukasz Kwiecień <kwiecien.lukasz@interia.pl>
2021-01-18 18:18:22 +0100

    Added book cover url helper

---
+++ src/ExBook.Extensions/BookCoverExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExBook.Extensions
+{
+   public static class BookCoverExtensions
+    {
+        private static string baseUrl = @"https://covers.openlibrary.org/b/id/{0}.jpg";
+        public static string? GetOriginalCoverUrl(string coverId)
+        {
+            if (!string.IsNullOrWhiteSpace(coverId))
+            {
+              string url =  String.Format(baseUrl, coverId);
+                return url;
+            }
+            else
+                return null;
+        }
+
+        public static string? GetLargeCoverUrl(string coverId)
+        {
+            if (!string.IsNullOrWhiteSpace(coverId))
+            {
+                string url = String.Format(baseUrl, coverId + "-L");
+                return url;
+            }
+            else
+                return null;
+        }
+
+        public static string? GetMediumCoverUrl(string coverId)
+        {
+            if (!string.IsNullOrWhiteSpace(coverId))
+            {
+                string url = String.Format(baseUrl, coverId+"-M");
+                return url;
+            }
+            else
+                return null;
+        }
+
+        public static string? GetSmallCoverUrl(string coverId)
+        {
+            if (!string.IsNullOrWhiteSpace(coverId))
+            {
+                string url = String.Format(baseUrl, coverId + "-S");
+                return url;
+            }
+            else
+                return null;
+        }
+
+    }
+}
